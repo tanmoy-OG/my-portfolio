39ea1212d3f899e4ee2a9b7bc84c699ff622e5d4
tanmoy-OG <official.tanmoychoudhury@protonmail.com>
2024-08-23 18:17:36 +0530

    added linting and @types dev-dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+export default [
+  {
+    rules: {
+      'no-console': 'off',
+      'no-unused-vars': 'off',
+      'no-undef': 'off',
+    },
+  }
+]
+
+// "extends": [
+//     "next",
+//     "next/core-web-vitals",
+//     "eslint:recommended",
+//     "plugin:import/recommended",
+//     "plugin:@typescript-eslint/recommended",
+//     "plugin:tailwindcss/recommended",
+//     "prettier"
+//   ],
