a8d4bb76444cff5488dd3d6262f94fcc80420b65
tanmoy-OG <official.tanmoychoudhury@protonmail.com>
2024-09-05 23:28:36 +0530

    added and configured shadcn/ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,9 @@
+import tailwindcssAnimate from 'tailwindcss-animate'
+
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  darkMode: ['class'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -12,9 +15,15 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
-      }
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      },
+      colors: {}
     }
   },
-  plugins: []
+  plugins: [tailwindcssAnimate]
 }
 export default config
