71d1f9922c4c476e896aa5b4badc7bd25613d890
tanmoy-OG <official.tanmoychoudhury@protonmail.com>
2025-08-19 21:02:46 +0530

    changelog 1. modularised homepage. 2. linted and formatted codebase.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,9 @@
-import Link from 'next/link';
-import NavigationLink from '@/components/landing/navigation-link';
+import Aside from '@/components/landing/aside';
 
 export default function Home() {
   return (
     <main className='min-h-screen bg-card flex'>
-      <aside className='bg-background basis-1/5 flex flex-col justify-between *:flex *:flex-col *:h-fit *:px-8 *:py-4 *:gap-2'>
-        <div className='!pt-8'>
-          <h1 className='text-lg text-primary'>Tanmoy_OG</h1>
-          <h1 className='text-lg/6'>Web Developer & Designer</h1>
-          <p className='text-xs'>
-            I build elegant and functional websites with fully responsive
-            design.
-          </p>
-        </div>
-        <div className='*:text-xs *:font-bold *:uppercase *:tracking-wide *:flex *:items-center *:gap-4 *:opacity-50 *:hover:opacity-100'>
-          <NavigationLink path='#about' linkName='About' />
-          <NavigationLink path='#experience' linkName='Experience' />
-          <NavigationLink path='#projects' linkName='Projects' />
-          <NavigationLink path='#skills' linkName='Achievements & Skills' />
-          <NavigationLink path='#blog' linkName='Blog' />
-          <NavigationLink path='#connect' linkName='Get in Touch' />
-        </div>
-        <div className='!pb-8'>
-          <h1>Coding Profiles</h1>
-          <div className='flex flex-col gap-1 *:text-xs'>
-            <Link href='path'>LeetCode</Link>
-            <Link href='path'>Codeforces</Link>
-            <Link href='path'>CodeChef</Link>
-            <Link href='path'>GeeksForGeeks</Link>
-          </div>
-        </div>
-      </aside>
+      <Aside />
       <div className='basis-4/5'></div>
     </main>
   );
