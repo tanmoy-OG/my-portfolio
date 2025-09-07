d77d4eb1652cba6ea8340ba13c57b60ace889c3f
tanmoy-OG <official.tanmoychoudhury@protonmail.com>
2025-09-07 16:58:34 +0530

    fixed ui components lucid-react import issues.

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
-import { CheckIcon } from 'lucide-react';
+import { LuCheck } from "react-icons/lu";
 import * as React from 'react';
 
 import { cn } from '@/lib/utils';
@@ -23,7 +23,7 @@ function Checkbox({
         data-slot='checkbox-indicator'
         className='flex items-center justify-center text-current transition-none'
       >
-        <CheckIcon className='size-3.5' />
+        <LuCheck className='size-3.5' />
       </CheckboxPrimitive.Indicator>
     </CheckboxPrimitive.Root>
   );
