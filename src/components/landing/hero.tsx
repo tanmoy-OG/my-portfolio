4dbcc4c755b640bff27e74de2c918dcd427545c4
tanmoy-OG <official.tanmoychoudhury@protonmail.com>
2025-10-31 23:37:13 +0530

    changelog: 1. added links to floating button social links in hero section in landing. 2. updated their hover effect styles.

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -4,6 +4,7 @@ import Stats from './hero-stats';
 import { getGithubContributions } from '@/lib/github';
 import projects from '@/assets/data/projects.json';
 import skills from '@/assets/data/skills.json';
+import Link from 'next/link';
 
 const Hero = async () => {
   const githubCommits = await getGithubContributions('tanmoy-OG');
@@ -29,10 +30,18 @@ const Hero = async () => {
           </h1>
           <h1 className='mt-4 opacity-20 font-cursive text-lg'>{'<div>'}</h1>
           <div className='flex gap-8 mx-8 mt-2'>
-            <FaLinkedin className='size-8' />
-            <FaXTwitter className='size-8' />
-            <FaGithub className='size-8' />
-            <FaInstagram className='size-8' />
+            <Link href="https://linkedin.com/in/tanmoy-choudhury/" className="hover:shadow-lg/50 hover:-translate-y-2 hover:scale-110 hover:text-accent transition-all">
+              <FaLinkedin className='size-8' />
+            </Link>
+            <Link href="https://x.com/tanmoy_OG" className="hover:shadow-lg/50 hover:-translate-y-2 hover:scale-110 hover:text-accent transition-all">
+              <FaXTwitter className='size-8' />
+            </Link>
+            <Link href="https://github.com/tanmoy-OG/" className="hover:shadow-lg/50 hover:-translate-y-2 hover:scale-110 hover:text-accent transition-all">
+              <FaGithub className='size-8' />
+            </Link>
+            <Link href="https://instagram.com/tanmoy_og/" className="hover:shadow-lg/50 hover:-translate-y-2 hover:scale-110 hover:text-accent transition-all">
+              <FaInstagram className='size-8' />
+            </Link>
           </div>
           <h1 className='mt-2 opacity-20 font-cursive text-lg'>{'</div>'}</h1>
           <h1 className='-ml-4 mt-4 opacity-20 font-cursive text-lg'>
