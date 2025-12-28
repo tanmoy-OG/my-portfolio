ecfb1189e98704233941bcc608f18dd1cbbb204b
tanmoy-OG <official.tanmoychoudhury@protonmail.com>
2025-12-28 19:54:06 +0530

    updated the connect-lonks style in connect section in landing

--- src/components/landing/connect-links.tsx
+++ src/components/landing/connect-links.tsx
@@ -5,8 +5,8 @@ import { MdOutlineEmail } from 'react-icons/md';
 
 const ConnectLink = () => {
     return (
-        <div className='w-full lg:w-1/3 flex flex-col gap-3 md:gap-4'>
-            <div className="flex flex-col md:flex-row lg:flex-col gap-3 md:gap-4">
+        <div className='w-full md:w-fit lg:w-1/3 flex flex-col md:flex-row lg:flex-col gap-3 md:gap-32 lg:gap-4 md:mx-auto lg:mx-0'>
+            <div className="flex flex-col gap-4">
                 <Link
                     href='mailto:connect@tanmoy-og.dev'
                     className='flex gap-3 md:gap-4 items-center z-20'
@@ -26,7 +26,9 @@ const ConnectLink = () => {
                         <h1 className='text-sm md:text-base'>{'+91 82401 06882'}</h1>
                         <div className='h-0.5 bg-accent opacity-0 group-hover:opacity-100 transition-all z-10'></div>
                     </div>
-                </Link></div><div className="flex flex-col md:flex-row lg:flex-col gap-3 md:gap-4">
+                </Link>
+            </div>
+            <div className="flex flex-col gap-4">
                 <Link
                     href='https://wa.me/918240106882'
                     className='flex gap-3 md:gap-4 items-center z-20'
@@ -46,7 +48,8 @@ const ConnectLink = () => {
                         <h1 className='text-sm md:text-base'>{'Kolkata, WB, India'}</h1>
                         <div className='h-0.5 bg-accent opacity-0 group-hover:opacity-100 transition-all z-10'></div>
                     </div>
-                </Link></div>
+                </Link>
+            </div>
         </div>
     );
 }
